5eafbc9736a06a6fc43a675dfde3b758862d0fdb
Daniel_Brand <idaniel95@me.com>
2020-06-15 20:59:34 +0200

    Remove que entities to remove and remove them at the end of the frame instead of during the frame.

--- MuseEngine/Muse/src/Core/ECS/Component/ComponentManager.h
+++ MuseEngine/Muse/src/Core/ECS/Component/ComponentManager.h
@@ -1,10 +1,11 @@
 ﻿#pragma once
-#include "CORE/Utilities/Defines.h"
+#include "Core/Utilities/Defines.h"
 #include "Core/ECS/Entity/Entity.h"
 
 #include <unordered_map>
+#include <vector>
 #include <memory>
-#include <variant>
+#include <string>
 
 namespace Muse
 {
@@ -19,10 +20,16 @@ namespace Muse
         static void Set(Entity a_Entity, T& a_Component);
 		static bool Exist(Entity a_Entity) { return s_Components.find(a_Entity) != s_Components.end(); }
 		static void OnEntityDestroy(int a_Entity);
+		static void Register(const std::string& a_RegisterName);
+        static void Update(float a_DeltaTime);
+        static void LateUpdate(float a_DeltaTime);
 
 	private:
         static std::unordered_map<int, T> s_Components;
+        static std::vector<int> s_EntitiesToRemove;
 		static bool s_Initiated;
+		static std::string s_Name;
+		static const std::string s_UnregisteredName;
 
 	};
 
@@ -30,8 +37,17 @@ namespace Muse
 	std::unordered_map<int, T> ComponentManager<T>::s_Components;
 
     template<class T>
+	std::vector<int> ComponentManager<T>::s_EntitiesToRemove;
+
+    template<class T>
 	bool ComponentManager<T>::s_Initiated = false;
-	
+
+    template<class T>
+	const std::string ComponentManager<T>::s_UnregisteredName = "Unregistered";
+
+    template<class T>
+    std::string ComponentManager<T>::s_Name = "";
+
 	template <class T>
 	void ComponentManager<T>::Add(Entity a_Entity, T a_Component)
 	{
@@ -39,12 +55,7 @@ namespace Muse
 		
 		if (!s_Initiated)
 		{
-  			Entity::s_DestroyEvent.Subscribe([](int a_Entity)
-			{ 
-				ComponentManager<T>::OnEntityDestroy(a_Entity);
-			});
-
-			s_Initiated = true;
+			Register(s_UnregisteredName);
 		}
 
 		std::pair<int, T> pair{ a_Entity, a_Component };
@@ -80,8 +91,45 @@ namespace Muse
 	{
 		if (Exist(a_Entity))
 		{
-            //s_Components.erase(a_Entity);
-			Remove(a_Entity);
+			s_EntitiesToRemove.push_back(a_Entity);
 		}
+	}
+
+	template<class T>
+	inline void ComponentManager<T>::Register(const std::string& a_RegisterName)
+	{
+        s_Name = a_RegisterName;
+        s_Initiated = true;
+
+        Entity::s_DestroyEvent.Subscribe([](int a_Entity)
+        {
+            ComponentManager<T>::OnEntityDestroy(a_Entity);
+        });
+
+        Application::Get().m_UpdateEvent.Subscribe([](float a_DeltaTime)
+        {
+            ComponentManager<T>::Update(a_DeltaTime);
+        });
+
+		Application::Get().m_LateUpdateEvent.Subscribe([](float a_DeltaTime)
+        {
+            ComponentManager<T>::LateUpdate(a_DeltaTime);
+        });
+	}
+
+	template<class T>
+	inline void ComponentManager<T>::Update(float a_DeltaTime)
+	{
+
+	}
+
+	template<class T>
+	inline void ComponentManager<T>::LateUpdate(float a_DeltaTime)
+	{
+		for (auto entity : s_EntitiesToRemove)
+		{
+			Remove(entity);
+		}
+		s_EntitiesToRemove.clear();
 	}
 }
