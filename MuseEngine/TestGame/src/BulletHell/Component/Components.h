4fbf7ac50aae4754be66a639c2136bbb53f81391
Daniel_Brand <idaniel95@me.com>
2020-06-15 11:44:19 +0200

    * [DeleteOutOfBoundsComponent] Added min max values.

--- MuseEngine/TestGame/src/BulletHell/Component/Components.h
+++ MuseEngine/TestGame/src/BulletHell/Component/Components.h
@@ -16,4 +16,10 @@ struct MoveForwardComponent
     float speed = 3.5f;
 };
 
-struct DeleteOutOfBoundsComponent { };
+struct DeleteOutOfBoundsComponent
+{ 
+    float minX = -5.0f;
+    float maxX = 5.0f;
+    float minY = -5.0f;
+    float maxY = 5.0f;
+};
