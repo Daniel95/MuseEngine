2208358878a5c818fe918202c4d81c3a12387ed7
Daniel_Brand <idaniel95@me.com>
2020-06-22 01:01:27 +0200

    Added exploding enemies.

--- MuseEngine/TestGame/src/BulletHell/Job/ObstacleCollisionJob.cpp
+++ MuseEngine/TestGame/src/BulletHell/Job/ObstacleCollisionJob.cpp
@@ -1,6 +1,7 @@
 #include "MusePCH.h"
 
 #include "Core/ECS/Entity/Entity.h"
+#include "Core/Utilities/Utilities.h"
 
 #include "ObstacleCollisionJob.h"
 #include "BulletHell/Component/Components.h"
@@ -38,7 +39,125 @@ void ObstacleCollisionJob::OnUpdate()
         )
     {
         Muse::Entity::Destroy(obstacleEntitiy1);
+        Muse::Entity::Destroy(obstacleEntitiy2);
     };
 
     RunCollision<ObstacleComponent, ObstacleComponent>(obstacleOnObstacleCollision);
+
+    auto projectileOnEnemyCollision = [](
+        int a_EnemyEntitiy,
+        EnemyComponent& a_EnemyComponent,
+        Muse::TransformComponent& a_TransformComponent1,
+        int a_ProjectileEntitiy,
+        ProjectileComponent& a_ProjectileComponent,
+        Muse::TransformComponent& a_TransformComponent2
+        )
+    {
+        Muse::Entity::Destroy(a_EnemyEntitiy);
+        Muse::Entity::Destroy(a_ProjectileEntitiy);
+
+        SpawnBullets(a_TransformComponent1.localPosition);
+    };
+
+    RunCollision<EnemyComponent, ProjectileComponent>(projectileOnEnemyCollision);
+}
+
+void ObstacleCollisionJob::SpawnBullets(glm::vec2 a_Position, float a_ProjectileSpeed)
+{
+    float offset = 1.0f;
+
+    //Up
+    {
+        Muse::TransformComponent projectileTransformComponent
+        {
+            glm::vec3(a_Position.x, a_Position.y + offset, 0),
+            glm::vec3(0.5f, 0.5f, 0.5f),
+        };
+
+        BulletHell::CreateProjectile(projectileTransformComponent, a_ProjectileSpeed);
+    }
+
+    //Up left
+    {
+        Muse::TransformComponent projectileTransformComponent
+        {
+            glm::vec3(a_Position.x - offset, a_Position.y + offset, 0),
+            glm::vec3(0.5f, 0.5f, 0.5f),
+            glm::vec3(0, 0, glm::radians(45.0f)),
+        };
+
+        BulletHell::CreateProjectile(projectileTransformComponent, a_ProjectileSpeed);
+    }
+
+    //left
+    {
+        Muse::TransformComponent projectileTransformComponent
+        {
+            glm::vec3(a_Position.x - offset, a_Position.y, 0),
+            glm::vec3(0.5f, 0.5f, 0.5f),
+            glm::vec3(0, 0, glm::radians(90.0f)),
+        };
+
+        BulletHell::CreateProjectile(projectileTransformComponent, a_ProjectileSpeed);
+    }
+
+    //Down left
+    {
+        Muse::TransformComponent projectileTransformComponent
+        {
+            glm::vec3(a_Position.x - offset, a_Position.y - offset, 0),
+            glm::vec3(0.5f, 0.5f, 0.5f),
+            glm::vec3(0, 0, glm::radians(135.0f)),
+        };
+
+        BulletHell::CreateProjectile(projectileTransformComponent, a_ProjectileSpeed);
+    }
+
+    //Down
+    {
+        Muse::TransformComponent projectileTransformComponent
+        {
+            glm::vec3(a_Position.x, a_Position.y - offset, 0),
+            glm::vec3(0.5f, 0.5f, 0.5f),
+            glm::vec3(0, 0, glm::radians(180.0f)),
+        };
+
+        BulletHell::CreateProjectile(projectileTransformComponent, a_ProjectileSpeed);
+    }
+
+    //Down right
+    {
+        Muse::TransformComponent projectileTransformComponent
+        {
+            glm::vec3(a_Position.x + offset, a_Position.y - offset, 0),
+            glm::vec3(0.5f, 0.5f, 0.5f),
+            glm::vec3(0, 0, glm::radians(225.0f)),
+        };
+
+        BulletHell::CreateProjectile(projectileTransformComponent, a_ProjectileSpeed);
+    }
+
+    //Right
+    {
+        Muse::TransformComponent projectileTransformComponent
+        {
+            glm::vec3(a_Position.x + offset, a_Position.y, 0),
+            glm::vec3(0.5f, 0.5f, 0.5f),
+            glm::vec3(0, 0, glm::radians(270.0f)),
+        };
+
+        BulletHell::CreateProjectile(projectileTransformComponent, a_ProjectileSpeed);
+    }
+
+    //Up Right
+    {
+        Muse::TransformComponent projectileTransformComponent
+        {
+            glm::vec3(a_Position.x + offset, a_Position.y + offset, 0),
+            glm::vec3(0.5f, 0.5f, 0.5f),
+            glm::vec3(0, 0, glm::radians(315.0f)),
+        };
+
+        BulletHell::CreateProjectile(projectileTransformComponent, a_ProjectileSpeed);
+    }
 }
--- MuseEngine/TestGame/src/BulletHell/Job/ObstacleCollisionJob.h
+++ MuseEngine/TestGame/src/BulletHell/Job/ObstacleCollisionJob.h
@@ -2,9 +2,13 @@
 
 #include "Core/ECS/Job/Job.h"
 
+#include "glm/glm.hpp"
+
 class ObstacleCollisionJob : public Muse::Job
 {
 public:
     virtual void OnUpdate() override;
+
+    static void SpawnBullets(glm::vec2 a_Position, float a_ProjectileSpeed = 10.0f);
 
 };
